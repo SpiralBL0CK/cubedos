6a15c008cd6a0031bf5e1617aafdd3b4c1ec63d8
Katie Niekrewicz <kxn05280@vsc.edu>
2024-07-08 17:22:25 -0400

    Style improvments

--- src/check/main.adb
+++ src/check/main.adb
@@ -39,6 +39,6 @@ begin
    loop
       delay until Next_Release;
       Next_Release := Next_Release + Ada.Real_Time.Milliseconds(1000);
-      return;
+      return; --change to exit?
    end loop;
 end Main;
